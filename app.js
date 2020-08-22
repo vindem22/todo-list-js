969141856f64398d1934652d48eaa77735277f67
Zhanbolat Bekmaganbetov <bekmaganbetov.janbolat@gmail.com>
2020-08-22 08:34:56 +0600

    Adding animation to the todoItem's

--- app.js
+++ app.js
@@ -39,7 +39,11 @@ function deleteCheck(event) {
   //Delete todo
   if (item.classList[0] === "trash-btn") {
     const todo = item.parentElement;
-    todo.remove();
+    //Animation
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
   }
 
   //Check mark
