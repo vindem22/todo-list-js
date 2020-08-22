9f9b8e37c396ddc67ccb4165df801e84df0f26de
Zhanbolat Bekmaganbetov <bekmaganbetov.janbolat@gmail.com>
2020-08-22 08:01:28 +0600

    Fixing trashButton class

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ function addTodo(event) {
   //Check trash button
   const trashButton = document.createElement("button");
   trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-  trashButton.classList.add("complete-btn");
+  trashButton.classList.add("trash-btn");
   todoDiv.appendChild(trashButton);
   //Append to todoList
   todoList.appendChild(todoDiv);
